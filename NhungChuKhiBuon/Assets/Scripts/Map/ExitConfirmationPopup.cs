2cce765eb6efe0f0c0234aa1ce85705a749c2117
hntquan0805 <hntquan0805@gmail.com>
2026-01-29 14:49:20 +0700

    merging

--- NhungChuKhiBuon/Assets/Scripts/Map/ExitConfirmationPopup.cs
+++ NhungChuKhiBuon/Assets/Scripts/Map/ExitConfirmationPopup.cs
@@ -21,12 +21,15 @@ public class ExitConfirmationPopup : MonoBehaviour
     [Header("Scene Settings")]
     public string mapChooseSceneName = "MapChoose";
 
-    private void Start()
+    private void Awake()
     {
         // Đảm bảo popup ẩn khi bắt đầu
         if (popupPanel != null)
             popupPanel.SetActive(false);
+    }
 
+    private void Start()
+    {
         // Gán sự kiện cho các nút
         if (pauseButton != null)
             pauseButton.onClick.AddListener(OnPauseClicked);
